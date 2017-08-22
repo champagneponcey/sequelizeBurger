d39b574e69a9f5c0ed0df5e77964f18e80d745e0
Jessica Thompson <champagneponcey@Jessicas-MacBook-Air.local>
2017-08-21 21:59:19 -0400

    burger.js update

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,19 @@
 
 
 module.exports = function(sequelize, DataTypes) {
-        var Burger = sequelize.define("Burger", {
-                all: {
-
-                },
-                create: {
-
-                },
-                update: {
-
-                }
-            }
-        };
-
-        Burger.sync();
+    var Burger = sequelize.define("Burger", {
+        all: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        create: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        update: {
+            type: DataTypes,
+            allowNull: false
+        }
+    });
+    Burger.sync();
+};
