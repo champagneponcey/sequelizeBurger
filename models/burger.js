45ed1287f34140b4d93c9a2fdf102c9a2ac811bf
Jessica Thompson <champagneponcey@Jessicas-MacBook-Air.local>
2017-08-21 21:41:06 -0400

    heroku deploy

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
 // var orm = require("../config/orm.js");
 
-var Burger = sequelize.define("Burger", {
-            all: function(cb) {
 
-            },
-            create: function(name, cb) {
+module.exports = function(sequelize, DataTypes) {
+        var Burger = sequelize.define("Burger", {
+                all: {
 
-            },
-            update: function(id, cb) {
-                var condition = "id=" + id;
+                },
+                create: {
 
+                },
+                update: {
+
+                }
             }
         };
 
         Burger.sync();
-
-        module.exports = Burger;
