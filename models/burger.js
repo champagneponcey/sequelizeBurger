312794c62d43e012647970813cb33f4654f3f2bf
Jess <jsscthomp@gmail.com>
2017-10-30 19:20:00 -0400

    updating burger model

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,30 @@
-// var orm = require("../config/orm.js");
+// Burger models
+
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
 
 
 module.exports = function(sequelize, DataTypes) {
     var Burger = sequelize.define("Burger", {
-        showAll: {
-            type: Sequelize.STRING,
+        burger_name: {
+            type: DataTypes.STRING,
             allowNull: false
         },
-        createBurger: {
-            type: Sequelize.STRING,
-            allowNull: false
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
         },
         updateBurger: {
             type: Sequelize,
             allowNull: false
+        }, 
+
+        classMethods: {
+            associate: function(models) {
+                Burger.hasOne(model.Customer);
+            }
         }
     });
 
-    Burger.sync({ force: false })
-        .then(() => {
-            // table created
-            return Burger.create({
-                showAll: [],
-                createBurger: '',
-                updateBurger: ''
-            });
-        });
+    return Burger;
 };
