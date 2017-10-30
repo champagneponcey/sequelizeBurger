fdba59130aded4ec3e29c0a0ee8e2770002a2414
Jess <jsscthomp@gmail.com>
2017-10-30 19:45:51 -0400

    added controller path to server. rearranged port variable

--- server.js
+++ server.js
@@ -7,48 +7,38 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('burgers', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      }
-});
-const routes = require("./routes");
 
-const User = sequelize.define('user', {
-
-})
+const routes = require("./controller/burgersController");
 
 // Sets up the Express App
 // =============================================================
-var PORT = process.env.PORT || 5000;
 var app = express();
 
+// add models
 var db = require('./models');
 
-app.set('port', PORT);
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 // parse application/x-www-form-urlencoded
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ 
+    defaultLayout: "main" 
+}));
 app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
-app.use(routes);
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
 // test sequelize to see if it is working
 // =============================================================
@@ -61,7 +51,9 @@ sequelize
     console.error('Unable to connect to the database:', err);
 });
 
-db.sequelize.sync({ force: true }).then(function() {
+// listen on port 5000
+var PORT = process.env.PORT || 5000;
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("Listening on port: " + PORT);
     });
